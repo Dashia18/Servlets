ffb9534df07128af49fc3a96f91ae433c2efc05c
Dashia <das-bobkova@yandex.ru>
2017-03-03 12:08:48 +0300

    LoginServlet: Init commit

--- src/myLoginServlet/LoginServlet.java
+++ src/myLoginServlet/LoginServlet.java
@@ -3,5 +3,32 @@ package myLoginServlet;
 /**
  * Created by Daria Serebryakova on 03.03.2017.
  */
-public class LoginServlet {
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        super.doPost(req, resp);
+    }
+
+
+
+
 }
